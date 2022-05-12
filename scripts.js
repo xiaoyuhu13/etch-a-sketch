47c74502ef15d50e9c820a229ea3b6f087ec2ef1
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-12 00:52:36 -0700

    Create new grid

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@
 const container = document.getElementById('container');
 
 //creating grid
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'block')
-    container.appendChild(div);
-    div.style.cssText = 'width:6.25%; height:6.25%;';
-};
+function createGrid(blockSize, gridSize){
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'block')
+        container.appendChild(div);
+        div.style.cssText = `width:${blockSize}%; height:${blockSize}%;`;
+    };
+}
+
+createGrid(6.25, 256);
 
 //change color once on hover
 const blocks = document.querySelectorAll('.block');
@@ -39,6 +43,31 @@ clear.addEventListener('click', function(){
     clearGrid(blocks)});
 
 //change grid size
+const grid = document.getElementById('grid');
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
+
+grid.addEventListener('click', function(){
+    let number = prompt("How many squares across would you like the Etch-a-Sketch to be?","");
+
+    let gridWidth = parseInt(number);
+    let gridSize = Math.pow(gridWidth,2);
+
+    if (typeof(gridWidth) != 'number') {
+        return alert('Please enter a number!');
+    } else if (gridWidth > 100) {
+        return alert('Do you want to crash your computer? Please enter a smaller number :(');
+    } else {
+        //clearing grid
+        removeAllChildNodes(container);
+        //recreating grid
+        let blockSize = 1/gridWidth * 100;
+        createGrid(blockSize, gridSize);
+    }
+});
 
 
