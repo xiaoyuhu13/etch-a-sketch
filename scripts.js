838115078e277bb6f314011ca40f6943d4912add
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-11 22:52:21 -0700

    Add color on hover and implement clear button

--- scripts.js
+++ scripts.js
@@ -4,26 +4,41 @@ const container = document.getElementById('container');
 //creating grid
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    div.setAttribute('class', 'block')
     container.appendChild(div);
     div.style.cssText = 'width:6.25%; height:6.25%;';
 };
 
 //change color once on hover
+const blocks = document.querySelectorAll('.block');
 
+function changeColor(block){
+    block.classList.add('blue');
+};
 
+for (let i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('mouseenter', function(){
+        changeColor(blocks[i]);
+    });
+};
 
-// const blocks = document.querySelectorAll('div');
+//clear grid
+const clear = document.getElementById('clear');
 
-// function changeColor(div){
-//     div.style.cssText = 'background-color = blue;'
-// };
+function removeColor(block) {
+    block.classList.remove('blue');
+};
 
-// for (let i = 0; i < blocks.length; i++) {
-//     blocks[i].addEventListener('mouseenter', function(){
-//         changeColor(blocks[i]);
-//     });
-// };
+function clearGrid(allBlocks){
+    for (let i = 0; i < blocks.length; i++){
+            removeColor(blocks[i]);
+        };
+    };
 
+clear.addEventListener('click', function(){
+    clearGrid(blocks)});
+
+//change grid size
 
 
 
